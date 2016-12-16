67c1fc29e1ba954d7c3f4bc0439b6b11ec35a8f2
Behram Radmanesh <behram11@gmail.com>
2016-12-16 16:55:34 -0500

    trying to connect various tables

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ Contains the sqlite tables and their attribute.
 """
 
 class Bed(db.Model):
-    __tablename__ = 'bed'
+    __tablename__ = 'beds'
     file_location = db.Column(db.Text(64), unique=True)
     label = db.Column(db.String(64), unique=True)
     date = db.Column(db.Integer(), unique=False)
@@ -23,7 +23,7 @@ class Bed(db.Model):
         return "'{0}', '{1}', '{2}'".format(self.file_location, self.label, self.md5)
 
 class Identity(db.Model):
-    __tablename__ = 'identity'
+    __tablename__ = 'identities'
     organism = db.Column(db.String(32), unique=False)
     technique = db.Column(db.String(32), unique=False)
     afactor = db.Column(db.String(32), unique=False)
@@ -31,7 +31,7 @@ class Identity(db.Model):
     cline = db.Column(db.String(32), unique=False)
     wld = db.Column(db.String(32), unique=False)
     dld = db.Column(db.String(32), unique=False)
-    md5 = db.Column(db.String(32), db.ForeignKey('bed.md5'), primary_key=True)
+    md5 = db.Column(db.String(32), db.ForeignKey('beds.md5'), primary_key=True)
 
     def __init__(self, organism, technique, afactor, tissue, cline, wld, dld, md5):
         self.organism = organism,
@@ -47,11 +47,11 @@ class Identity(db.Model):
         return "'{0}', '{1}'".format(self.organism, self.md5)
 
 class Colocalization(db.Model):
-    __tablename__ = 'colocalization'
+    __tablename__ = 'colocalizations'
     method = db.Column(db.String(64), unique=False, primary_key=True)
     value = db.Column(db.Integer(), unique=False)
-    md5_1 = db.Column(db.String(32), db.ForeignKey('bed.md5'), primary_key=True)
-    md5_2 = db.Column(db.String(32), db.ForeignKey('bed.md5'), primary_key=True)
+    md5_1 = db.Column(db.String(32), db.ForeignKey('beds.md5'), primary_key=True)
+    md5_2 = db.Column(db.String(32), db.ForeignKey('beds.md5'), primary_key=True)
 
     def __init__(self, method, value, md5_1, md5_2):
         self.method = method
